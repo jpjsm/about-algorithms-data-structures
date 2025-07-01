665d361e46b0cef6b092c628ae48c46989b72b5d
Juan Pablo 'JP' Jofre <juanpablo.jofre@live.com>
2025-07-01 13:54:01 -0700

    Many chapters  completed

---
+++ sedgewick-cpp/ch05-recursion-and-trees/CS/binarytree.test/binarytreetests.cs
@@ -0,0 +1,87 @@
+namespace binarytree.test;
+
+public class BinaryTreeTests
+{
+    [Theory]
+    [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 })]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, new int[] { 0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9 })]
+    public void BuildTreeTest(int[] inserts, int[] expected)
+    {
+        BinaryTree<int> bt = new BinaryTree<int>(inserts[0]);
+        for (int i = 1; i < inserts.Length; i++)
+        {
+            bt.Insert(inserts[i]);
+        }
+        Assert.Equal(expected.ToList(), bt.ToListAscending());
+    }
+
+    [Theory]
+    [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9)]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 9)]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9)]
+    public void CheckTreeHeightTest(int[] inserts, int expected)
+    {
+        BinaryTree<int> bt = new BinaryTree<int>(inserts[0]);
+        for (int i = 1; i < inserts.Length; i++)
+        {
+            bt.Insert(inserts[i]);
+        }
+        Assert.Equal(expected, bt.Height);
+    }
+
+    [Theory]
+    [InlineData(new int[] { 0 }, 0, 0)]
+    [InlineData(new int[] { 0, 1 }, 1, 1)]
+    [InlineData(new int[] { 0, 1, 2 }, 2, 1)]
+    [InlineData(new int[] { 0, 1, 2, 3 }, 3, 2)]
+    [InlineData(new int[] { 0, 1, 2, 3, 4 }, 4, 2)]
+    [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 5, 2)]
+    [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6 }, 6, 2)]
+    [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6, 7 }, 7, 3)]
+    [InlineData(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9, 3)]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 }, 9, 3)]
+    [InlineData(new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, 9, 3)]
+    public void CheckTreeBalancedHeightTest(int[] inserts, int expectedunbalanced, int expectedbalanced)
+    {
+        BinaryTree<int> bt = new BinaryTree<int>(inserts[0]);
+        for (int i = 1; i < inserts.Length; i++)
+        {
+            bt.Insert(inserts[i]);
+        }
+        Assert.Equal(expectedunbalanced, bt.Height);
+        bt.Balance();
+        Assert.Equal(expectedbalanced, bt.Height);
+    }
+
+    public static IEnumerable<object[]> ToListLevelTraverseTestData =>
+        [
+            [new int[] { 0 }, -1, new List<(int level, int item)> { (0,0) }],
+            [new int[] { 0 }, 0, new List<(int level, int item)> { (0,0) }],
+            [new int[] { 0 }, 1, new List<(int level, int item)> {  }],
+            [
+                new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                -1,
+                new List<(int level, int item)> { (0,9), (0,9), (1,8), (1,8), (2,7), (2,7), (3,6), (3,6), (4,5), (4,5), (5,4), (5,4), (6,3), (6,3),  (7,2), (7,2), (8,1), (8,1), (9,0), (9,0),}
+            ],
+            [
+                new int[] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                7,
+                new List<(int level, int item)> { (7,2), (7,2)}
+            ],
+        ];
+
+    [Theory]
+    [MemberData(nameof(ToListLevelTraverseTestData))]
+    public void ToListLevelTraverseTest(int[] inserts, int level, List<(int level, int item)> expected)
+    {
+        BinaryTree<int> bt = new BinaryTree<int>(inserts[0]);
+        for (int i = 1; i < inserts.Length; i++)
+        {
+            bt.Insert(inserts[i]);
+        }
+
+        var actual = bt.ToListLevelTraverse(level);
+        Assert.Equal(expected, actual);
+    }
+}
---
+++ sedgewick-cpp/ch05-recursion-and-trees/CS/binarytree/binarytree.cs
@@ -0,0 +1,200 @@
+﻿namespace binarytree;
+
+public class BinaryTree<T> where T : IComparable
+{
+    private List<T> values = new List<T>();
+    private BinaryTree<T>? left = null;
+    private BinaryTree<T>? right = null;
+
+    public T Value => values[0];
+    public int Height => height(this);
+
+    public BinaryTree(T value)
+    {
+        values.Add(value);
+    }
+
+    public void Insert(T value)
+    {
+        int comparison = value.CompareTo(Value);
+
+        switch (comparison)
+        {
+            case 0:
+                values.Add(value);
+                break;
+
+            case -1:
+                if (left == null)
+                {
+                    left = new BinaryTree<T>(value);
+                }
+                else
+                {
+                    left.Insert(value);
+                }
+                break;
+
+            case 1:
+                if (right == null)
+                {
+                    right = new BinaryTree<T>(value);
+                }
+                else
+                {
+                    right.Insert(value);
+                }
+                break;
+
+            default:
+                throw new ApplicationException("Unexpected 'value.CompareTo(Value)' result");
+
+        }
+    }
+
+    public List<T> ToListAscending()
+    {
+        List<T> results = new List<T>();
+        if (left != null) results.AddRange(left.ToListAscending());
+        results.AddRange(values);
+        if (right != null) results.AddRange(right.ToListAscending());
+        return results;
+    }
+
+    public List<T> ToListDescending()
+    {
+        List<T> results = new List<T>();
+        if (right != null) results.AddRange(right.ToListAscending());
+        results.AddRange(values);
+        if (left != null) results.AddRange(left.ToListAscending());
+        return results;
+    }
+
+    public List<(int level,T item)> ToListLevelTraverse(int whichlevel=-1)
+    {
+        List<(int level,T item)> results = [];
+        Dictionary<int, List<T>> levelnodes = [];
+        leveltraverse(this, 0, levelnodes);
+
+        if (whichlevel == -1) // list all levels in ascending order 
+        {
+            foreach (int level in levelnodes.Keys.Order())
+            {
+                foreach (T item in levelnodes[level])
+                {
+                    results.Add((level, item));
+                }
+            }
+        }
+        else
+        {
+            if (levelnodes.ContainsKey(whichlevel))
+            {
+                foreach (T item in levelnodes[whichlevel])
+                {
+                    results.Add((whichlevel, item));
+                }
+            }
+        }
+
+        return results;
+    }
+
+    public void Balance()
+    {
+        T[] sortedlist = [.. this.ToListAscending()];
+        HashSet<T> uniques = new HashSet<T>();
+        List<T> insertlist = new List<T>();
+        List<T> duplicates = new List<T>();
+
+        foreach (T item in sortedlist)
+        {
+            if (!uniques.Contains(item))
+            {
+                insertlist.Add(item);
+            }
+            else
+            {
+                duplicates.Add(item);
+            }
+        }
+
+        List<int> insertindexes = lrsplits(0, insertlist.Count - 1);
+
+        this.left = null;
+        this.right = null;
+        this.values = new List<T>();
+        values.Add(insertlist[insertindexes[0]]);
+        insertindexes.RemoveAt(0);
+        foreach (int index in insertindexes)
+        {
+            this.Insert(insertlist[index]);
+        }
+
+        foreach (T dup in duplicates)
+        {
+            this.Insert(dup);
+        }
+    }
+    private static void leveltraverse(BinaryTree<T> n, int level, Dictionary<int, List<T>> levelnodes)
+    {
+        if (!levelnodes.ContainsKey(level))
+        {
+            levelnodes[level] = [];
+        }
+
+        levelnodes[level].AddRange(n.values);
+        if (n.left != null) leveltraverse(n.left, level + 1, levelnodes);
+        if (n.right != null) leveltraverse(n.right, level + 1, levelnodes);
+    }
+
+    private static List<int> lrsplits(int l, int r)
+    {
+        List<int> results = new List<int>();
+        if (l >= r)
+        {
+            results.Add(l);
+            return results;
+        }
+
+        if (l + 1 == r)
+        {
+            results.Add(r);
+            results.Add(l);
+            return results;
+        }
+
+        if (l + 2 == r)
+        {
+            results.Add(l + 1);
+            results.Add(r);
+            results.Add(l);
+            return results;
+        }
+
+        int mid = (l + r) >> 1;
+        results.Add(mid);
+        results.AddRange(lrsplits(mid + 1, r));
+        results.AddRange(lrsplits(l, mid - 1));
+
+        return results;
+    }
+
+    private static int height(BinaryTree<T> n)
+    {
+        int leftheight = 0;
+        int rightheight = 0;
+
+        if (n.left != null)
+        {
+            leftheight = height(n.left) + 1;
+        }
+
+        if (n.right != null)
+        {
+            rightheight = height(n.right) + 1;
+        }
+
+        return leftheight > rightheight ? leftheight : rightheight;
+    }
+}
